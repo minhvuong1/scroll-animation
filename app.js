2270c7d09b2fdb423741241fe966d79364d7ea55
Minh Vuong <minhvuong@live.com>
2021-09-29 21:47:13 +1000

    added scroll functionality

---
+++ app.js
@@ -0,0 +1,39 @@
+const balloon = document.getElementById('air-balloon');
+const cloudOne = document.getElementById('cloud-one');
+const cloudTwo = document.getElementById('cloud-two');
+const cloudThree = document.getElementById('cloud-three');
+const cloudFour = document.getElementById('cloud-four');
+const cloudFive = document.getElementById('cloud-five');
+const javascript = document.getElementById('javascript');
+const react = document.getElementById('react');
+const graphql = document.getElementById('graphql');
+
+function move() {
+    const incrementer = window.scrollY
+    
+    // The '10' refers to the height from the bottom of the sky section. 
+    // The '10' refers to starting position
+    balloon.style.bottom = 10 + (incrementer * 0.1) + "%";
+
+    cloudOne.style.bottom = 40 + incrementer * 0.12 + "%";
+    cloudOne.style.left = 75 + incrementer * 0.1 + "%";
+
+    cloudTwo.style.bottom = 80 + incrementer * 0.14 + "%";
+    cloudTwo.style.left = 70 + incrementer * 0.15 + "%";
+
+    cloudThree.style.bottom = 60 + incrementer * 0.14 + "%";
+    // Put minus in -0.15 so it goes in opposite direction
+    cloudThree.style.left = 0 + incrementer * -0.15 + "%";
+
+    cloudFour.style.bottom = 70 + incrementer * 0.16 + "%";
+    cloudFour.style.left = 20 + incrementer * -0.15 + "%";
+
+    cloudFive.style.bottom = 60 + incrementer * 0.2 + "%";
+    cloudFive.style.left = 60 + incrementer * 0.16 + "%";
+
+    javascript.style.left = 20 + incrementer * -2.2 + "%";
+    react.style.left = 26 + incrementer * -1.5 + "%";
+    graphql.style.left = 32 + incrementer * -1 + "%";
+}
+
+window.addEventListener('scroll', move)
